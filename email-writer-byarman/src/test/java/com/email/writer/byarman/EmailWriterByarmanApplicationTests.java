37c5510c3201b3dd5a848c6ff1395114697342ce
Mohd Arman Tyagi <mohdarmantyagi.bca2023@imsuc.ac.in>
2025-08-27 00:03:49 +0530

    Initial commit - backend

---
+++ email-writer-byarman/src/test/java/com/email/writer/byarman/EmailWriterByarmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.writer.byarman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterByarmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
